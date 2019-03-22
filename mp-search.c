2e3dd3f3c81f2714f67e7b3391d2283db8dfe41b
Jacob Chappell <chappellind@gmail.com>
2019-03-21 23:47:00 -0400

    Add state dumping.

--- mp-search.c
+++ mp-search.c
@@ -9,9 +9,18 @@
 #define BUFFER_SZ 1024
 
 /**
+ * Application state: stored globally so that it can be dumped on exit.
+ */
+static char buffer[BUFFER_SZ];
+static int digits = 0;
+static int d = 0;
+static char min = '0';
+static mpz_t acc;
+
+/**
  * Generator callback function pointer which can check generated integers.
  */
-typedef void (*generator_cb)(const char *, void *);
+typedef int (*generator_cb)(const char *, void *);
 
 /**
  * Return the multiplicative persistence of the integer represented in base 10
@@ -38,39 +47,7 @@ static int check(const char * x, mpz_t * acc)
 
 /**
  * Generate candidate integers iteratively.
- */
-static void generate(char * buffer, int digits, int d, char min,
-        generator_cb cb, void * data)
-{
-    char i = 0;
-    while (1) {
-        if (d >= digits) {
-            buffer[d] = '\0';
-            cb(buffer, data);
-            do {
-                d -= 1;
-                if (d < 0) {
-                    return;
-                }
-                min = buffer[d] + 1;
-            } while (min > '9');
-        }
-        for (i = min; i <= '9'; i++) {
-            buffer[d] = i;
-            d += 1;
-            min = i;
-            break;
-        }
-    }
-}
-
-void checker(const char * x, void * data)
-{
-    int p = check(x, (mpz_t *) data);
-    printf("Checker for %s is %d\n", x, p);
-}
-
-/**
+ *
  * Pairs not to include for efficiency:
  * 2x2=4
  * 2x3=6
@@ -79,13 +56,55 @@ void checker(const char * x, void * data)
  * 3x3=9
  * 4x2=8
  */
+static void generate(char * buffer, int * digits, int * d, char * min,
+        generator_cb cb, void * data)
+{
+    char i = 0;
+    while (1) {
+        if (*d >= *digits) {
+            buffer[*d] = '\0';
+            if (0 != cb(buffer, data)) {
+                exit(2);
+            }
+            do {
+                *d -= 1;
+                if (*d < 0) {
+                    return;
+                }
+                *min = buffer[*d] + 1;
+            } while (*min > '9');
+        }
+        for (i = *min; i <= '9'; i++) {
+            buffer[*d] = i;
+            *d += 1;
+            *min = i;
+            break;
+        }
+    }
+}
+
+int checker(const char * x, void * data)
+{
+    /*static int checks = 0;*/
+    int p = check(x, (mpz_t *) data);
+    printf("Checker for %s is %d\n", x, p);
+    /*checks++;*/
+    /*return checks >= 1000;*/
+    return 0;
+}
+
+void dump_state(void)
+{
+    mpz_clear(acc);
+    if (d < 0) {
+        printf("Done!\n");
+    } else {
+        printf("State: %d \"%s\" %d %c\n", digits, buffer, d, min);
+    }
+}
+
 int main(int argc, char * argv[])
 {
-    char buffer[BUFFER_SZ];
-    mpz_t acc;
-    int digits = 0;
-    int d = 0;
-    char min = '0';
     if (argc < 2) {
         fprintf(stderr,
                 "Usage: %s <digits> [<seed>] [<d>] [<min>]\n",
@@ -132,9 +151,9 @@ int main(int argc, char * argv[])
     }
     printf("Initialized state: digits = %d, buffer = \"%s\", d = %d, min = %c\n",
             digits, buffer, d, min);
+    atexit(dump_state);
     /* Run the generator with the initialized state. */
     mpz_init(acc);
-    generate(buffer, digits, d, min, checker, &acc);
-    mpz_clear(acc);
+    generate(buffer, &digits, &d, &min, checker, &acc);
     return 0;
 }
